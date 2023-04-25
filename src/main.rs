40fde48e9e683bc2add1d59240f49c1c3deecf6d
s <a@b.c>
2023-04-24 21:36:55 -0700

    Rename window, formatted by rustfmt

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,3 @@
-
 use std::ops::Add;
 use std::ops::AddAssign;
 
@@ -18,65 +17,81 @@ const PLAYFIELD_HEIGHT: f64 = 480.0 / CELL_SIZE;
 const UPDATE_INTERVAL: f64 = 1.0 / 4.0;
 
 struct GameState {
-	spos: [f64; 2],
-	sdir: [f64; 2],
-	tail: Vec<[f64; 2]>,
-	tail_length: isize,
-	fpos: [f64; 2],
-	interval_time: f64,
+    spos: [f64; 2],
+    sdir: [f64; 2],
+    tail: Vec<[f64; 2]>,
+    tail_length: isize,
+    fpos: [f64; 2],
+    interval_time: f64,
 }
 
 impl GameState {
-	fn new() -> Self {
-		GameState {
-			spos: [0.0; 2],
-			sdir: [0.0; 2],
-			tail: vec![],
-			tail_length: 0,
-			fpos: [0.0; 2],
-			interval_time: 0.0,
-		}
-	}
+    fn new() -> Self {
+        GameState {
+            spos: [0.0; 2],
+            sdir: [0.0; 2],
+            tail: vec![],
+            tail_length: 0,
+            fpos: [0.0; 2],
+            interval_time: 0.0,
+        }
+    }
 
-	fn randomize_food(&mut self) {
+    fn randomize_food(&mut self) {}
 
-	}
+    fn update(&mut self, dt: f64) {
+        self.interval_time += dt;
+        if self.interval_time < UPDATE_INTERVAL {
+            return;
+        }
+        self.interval_time -= UPDATE_INTERVAL;
 
-	fn update(&mut self, dt: f64) {
-		self.interval_time += dt;
-		if self.interval_time < UPDATE_INTERVAL { return; }
-		self.interval_time -= UPDATE_INTERVAL;
+        self.spos[0] += self.sdir[0];
+        self.spos[1] += self.sdir[1];
+        // branchless wrapping
+        self.spos[0] += PLAYFIELD_WIDTH
+            * ((self.spos[0] < 0.0) as u8 as f64
+                + -((self.spos[0] > PLAYFIELD_WIDTH - 1.0) as u8 as f64));
+        self.spos[1] += PLAYFIELD_HEIGHT
+            * ((self.spos[1] < 0.0) as u8 as f64
+                + -((self.spos[1] > PLAYFIELD_HEIGHT - 1.0) as u8 as f64));
+    }
 
-		self.spos[0] += self.sdir[0];
-		self.spos[1] += self.sdir[1];
-		// branchless wrapping
-		self.spos[0] += PLAYFIELD_WIDTH * ((self.spos[0] < 0.0) as u8 as f64 + -((self.spos[0] > PLAYFIELD_WIDTH - 1.0) as u8 as f64));
-		self.spos[1] += PLAYFIELD_HEIGHT * ((self.spos[1] < 0.0) as u8 as f64 + -((self.spos[1] > PLAYFIELD_HEIGHT - 1.0) as u8 as f64));
-	}
+    fn button_event(&mut self, args: ButtonArgs) {
+        if args.state == ButtonState::Release {
+            return;
+        }
+        match args.button {
+            Button::Keyboard(Key::D) => self.sdir = [1.0, 0.0],
+            Button::Keyboard(Key::A) => self.sdir = [-1.0, 0.0],
+            Button::Keyboard(Key::S) => self.sdir = [0.0, 1.0],
+            Button::Keyboard(Key::W) => self.sdir = [0.0, -1.0],
+            _ => {}
+        }
+    }
 
-	fn button_event(&mut self, args: ButtonArgs) {
-		if args.state == ButtonState::Release { return; }
-		match args.button {
-			Button::Keyboard(Key::D) => self.sdir = [1.0, 0.0],
-			Button::Keyboard(Key::A) => self.sdir = [-1.0, 0.0],
-			Button::Keyboard(Key::S) => self.sdir = [0.0, 1.0],
-			Button::Keyboard(Key::W) => self.sdir = [0.0, -1.0],
-			_ => {}
-		}
-	}
-
-	fn render(&self, args: RenderArgs, gl: &mut GlGraphics) {
-		use graphics::{clear, rectangle};
-		gl.draw(args.viewport(), |c, g| {
-			clear(BG_COLOR, g);
-			rectangle(SNAKE_COLOR, [self.spos[0] * CELL_SIZE, self.spos[1] * CELL_SIZE, CELL_SIZE, CELL_SIZE], c.transform, g);
-		})
-	}
+    fn render(&self, args: RenderArgs, gl: &mut GlGraphics) {
+        use graphics::{clear, rectangle};
+        gl.draw(args.viewport(), |c, g| {
+            clear(BG_COLOR, g);
+            rectangle(
+                SNAKE_COLOR,
+                [
+                    self.spos[0] * CELL_SIZE,
+                    self.spos[1] * CELL_SIZE,
+                    CELL_SIZE,
+                    CELL_SIZE,
+                ],
+                c.transform,
+                g,
+            );
+        })
+    }
 }
 
 fn main() {
     let opengl = OpenGL::V3_2;
-    let mut window: GlfwWindow = WindowSettings::new("piston-example-user_input", [640, 480])
+    let mut window: GlfwWindow = WindowSettings::new("Snake", [640, 480])
         .exit_on_esc(true)
         .resizable(false)
         .graphics_api(opengl)
@@ -85,19 +100,19 @@ fn main() {
 
     let gl = &mut GlGraphics::new(opengl);
 
-	let mut gamestate = GameState::new();
+    let mut gamestate = GameState::new();
 
     let mut events = Events::new(EventSettings::new());
     while let Some(e) = events.next(&mut window) {
         // activates on press or release
         if let Some(args) = e.button_args() {
-			gamestate.button_event(args);
+            gamestate.button_event(args);
         }
         if let Some(args) = e.render_args() {
-			gamestate.render(args, gl);
+            gamestate.render(args, gl);
         }
         if let Some(args) = e.update_args() {
-			gamestate.update(args.dt);
-		}
+            gamestate.update(args.dt);
+        }
     }
 }
